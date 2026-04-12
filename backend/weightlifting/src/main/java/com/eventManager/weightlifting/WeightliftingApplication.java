e2bea6926ff5943c89ce4e496e1f9720c376942a
WojciechFut <wojowany@gmail.com>
2026-04-12 13:51:30 +0200

    initial backend commit

---
+++ backend/weightlifting/src/main/java/com/eventManager/weightlifting/WeightliftingApplication.java
@@ -0,0 +1,13 @@
+package com.eventManager.weightlifting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeightliftingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeightliftingApplication.class, args);
+	}
+
+}
--- backend/weightlifting/src/test/java/com/eventManager/weightlifting/WeightliftingApplicationTests.java
+++ backend/weightlifting/src/test/java/com/eventManager/weightlifting/WeightliftingApplicationTests.java
@@ -1,10 +1,10 @@
-package com.playground.backend;
+package com.eventManager.weightlifting;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class WeightliftingApplicationTests {
 
 	@Test
 	void contextLoads() {
