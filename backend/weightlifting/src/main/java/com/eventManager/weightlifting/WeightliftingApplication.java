88b2d9fbdef2d6e0f434f23791c702689a58cc96
WojciechFut <wojowany@gmail.com>
2026-04-12 15:48:35 +0200

    chore: new Dockerfile for backend, docker compose changes

--- backend/weightlifting/src/main/java/com/eventManager/weightlifting/WeightliftingApplication.java
+++ backend/weightlifting/src/main/java/com/eventManager/weightlifting/WeightliftingApplication.java
@@ -3,6 +3,8 @@ package com.eventManager.weightlifting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class WeightliftingApplication {
 
